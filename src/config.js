042c363d5b10f8f9cfcad62374a7ca4487de643f
MichaelValwin <macm241192@gmail.com>
2022-07-01 18:38:48 +0200

    Added some const vars

--- src/config.js
+++ src/config.js
@@ -11,5 +11,8 @@ module.exports = {
 	SOCKS_PROXY: 'socks5://192.168.1.151:9050', // Ex: 'socks5://192.168.1.150:9050'
 
 	NITTER_WEBSITE: 'https://twitter.censors.us/',
-	TWITTER_LISTS: './nitterList.json'
+	TWITTER_LISTS: './nitterList.json',
+
+	COOKIE_MAX_AGE: 1000 * 60 * 60 * 24 * 30, // 30 days
+	UPDATE_TIME: 1000 * 60 * 60 * 6 // 6h
 }
