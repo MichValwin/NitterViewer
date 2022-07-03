198054c85095af94f7f4e55b157693ab657440aa
MichaelValwin <macm241192@gmail.com>
2022-07-03 21:04:24 +0200

    Remove old log file and add date to them

--- src/logger.js
+++ src/logger.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const LOGGIN_FILE_NAME = 'log';
 const MAX_SIZE_PER_FILE = 24000000; //24MB
+const MAX_LOGS = 4;
 
 const LOG_LEVEL_ERROR = 0;
 const LOG_LEVEL_WARN = 1;
@@ -13,6 +14,11 @@ var currentLogfileName;
 var logDirectory;
 var currentLogLevel = LOG_LEVEL_INFO;
 
+function fillLeftZero(str, lengthNeeded) {
+	while(str.length < lengthNeeded)str = '0' + str;
+	return str;
+}
+
 exports.initLogger = function(logDir, logLevel) {
 	logDirectory = logDir;
 	currentLogLevel = logLevel;
@@ -22,15 +28,63 @@ exports.initLogger = function(logDir, logLevel) {
 		fs.mkdirSync(logDirectory);
 	}
 
-	// Get all files in dir and count log files
-	let filesInLogDir = fs.readdirSync(logDirectory);
-	let logFiles = filesInLogDir.filter(fileName => {
-		if(fileName.indexOf(LOGGIN_FILE_NAME) != -1)return true;
-	});
-	let numLogs = logFiles.length;
+	let currentTime = new Date();
+	let currentTimeFormated = currentTime.getFullYear() + '-' + fillLeftZero(String(currentTime.getMonth()), 2) + '-' + fillLeftZero(String(currentTime.getDate()), 2);
 
+	let arrFilesInLogDir = fs.readdirSync(logDirectory);
+
+	// Count num of logs
+	let allLogsFileNames = arrFilesInLogDir.filter(fileName => {
+		if(fileName.indexOf('.log') != -1)return true;
+	});
+	let numLogs = allLogsFileNames.length;
+
+	console.log(allLogsFileNames);
+
+	// Remove one old file
+	if(numLogs >= MAX_LOGS) {
+		let oldestFile = null;
+		let oldestfileModificationTime = null;
+		allLogsFileNames.forEach(file => {
+			let fileModificationTime = fs.statSync(logDirectory + file).mtimeMs;
+			if(oldestfileModificationTime == null || fileModificationTime < oldestfileModificationTime) {
+				oldestFile = file;
+				oldestfileModificationTime = fileModificationTime;
+			}
+		});
+		fs.unlinkSync(logDirectory + oldestFile);
+	}
+
+	// Count repeated log files
+	arrFilesInLogDir = fs.readdirSync(logDirectory);
+	allLogsFileNames = arrFilesInLogDir.filter(fileName => {
+		if(fileName.indexOf('.log') != -1)return true;
+	});
+	let repeatedTodayLogFiles = allLogsFileNames.filter(fileName => {
+		if(fileName.indexOf(LOGGIN_FILE_NAME + currentTimeFormated) != -1)return true;
+	});
+
+	// Get first number
+	let smallerAvalibleFileNum = 0;
+	let arrExistingNum = new Array(MAX_LOGS + 1);
+	arrExistingNum.fill(false);
+	repeatedTodayLogFiles.forEach(fileName => {
+		let fileNum = parseInt(fileName.match(/.(\d*?).log/)[1]);
+		if(!isNaN(fileNum)) {
+			arrExistingNum[fileNum] = true;
+		}
+	});
+	console.log(arrExistingNum);
+	for(let i = 0; i < arrExistingNum.length; i++) {
+		if(!arrExistingNum[i]) {
+			smallerAvalibleFileNum = i;
+			break;
+		}
+	}
+
+	currentLogfileName = LOGGIN_FILE_NAME + currentTimeFormated + '.' +  smallerAvalibleFileNum + '.log';
+	
 	// Init new file
-	currentLogfileName = LOGGIN_FILE_NAME + String(numLogs) + '.log';
 	fs.writeFileSync(logDirectory + currentLogfileName, '');
 }
 
