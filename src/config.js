07ad8322e107b66fa5978e8810643502fc192ec1
MichaelValwin <macm241192@gmail.com>
2022-07-03 21:03:07 +0200

    Add Log_level constant

--- src/config.js
+++ src/config.js
@@ -1,15 +1,16 @@
 module.exports = {
     PORT: 8020,
 	API_ENDPOINT: '/v1',
-	LOGGIN_DIRECTORY: './logs/',
+	LOG_DIRECTORY: './logs/',
+	LOG_LEVEL: 3,
 
 	DEBUG: false,
 	PASSWORD_FILE: './password.txt',
 	SALT_ROUNDS: 14,
-	
+		
 	DOWNLOAD_FOLDER: './download/',
 
-	SOCKS_PROXY: 'socks5://192.168.1.151:9050', // Ex: 'socks5://192.168.1.150:9050'
+	SOCKS_PROXY: 'socks5://192.168.1.151:9050', // Ex: 'socks5://192.168.1.151:9050'
 
 	NITTER_WEBSITE: 'https://twitter.censors.us/',
 	TWITTER_LISTS: './nitterList.json',
