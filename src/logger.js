7a361739fd0c6e226fef1587faba526a9c6a3019
MichaelValwin <macm241192@gmail.com>
2022-07-06 22:43:43 +0200

    Archive logs with date as filename

--- src/logger.js
+++ src/logger.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 
-const LOGGIN_FILE_NAME = 'log';
+const LOG_ARCHIVE_PREFIX = 'log';
+const LOG_SUFIX = '.log';
 const MAX_SIZE_PER_FILE = 24000000; //24MB
-const MAX_LOGS = 4;
+const MAX_LOGS = 7;
 
 const LOG_LEVEL_ERROR = 0;
 const LOG_LEVEL_WARN = 1;
@@ -10,79 +11,104 @@ const LOG_LEVEL_INFO = 2;
 const LOG_LEVEL_VERBOSE = 3;
 const LOG_LEVEL_DEBUG = 4;
 
-var currentLogfileName;
+const CURRENT_LOG_NAME = 'latest'; 
+
 var logDirectory;
 var currentLogLevel = LOG_LEVEL_INFO;
+var currentLogfileName;
 
 function fillLeftZero(str, lengthNeeded) {
 	while(str.length < lengthNeeded)str = '0' + str;
 	return str;
 }
 
+function getLogFileNames(logDirectory) {
+	let arrFilesInLogDir = fs.readdirSync(logDirectory);
+
+	// Count num of logs
+	return arrFilesInLogDir.filter(filename => {
+		if(filename.indexOf(LOG_SUFIX) != -1)return true;
+	});
+}
+
+function removeOldestLog(logDirectory, arrLogFilenames) {
+	let oldestFile = null;
+	let oldestfileModificationTime = null;
+	arrLogFilenames.forEach(filename => {
+		let fileModificationTime = fs.statSync(logDirectory + filename).mtimeMs;
+		if(oldestfileModificationTime == null || fileModificationTime < oldestfileModificationTime) {
+			oldestFile = filename;
+			oldestfileModificationTime = fileModificationTime;
+		}
+	});
+	fs.unlinkSync(logDirectory + oldestFile);
+}
+
+function getLogFilesNamesContainsString(logDirectory, strCompare) {
+	// Count repeated log files
+	let arrFilesInLogDir = fs.readdirSync(logDirectory);
+	return arrFilesInLogDir.filter(fileName => {
+		if(fileName.indexOf(LOG_SUFIX) != -1 && fileName.indexOf(strCompare) != -1)return true;
+	});
+}
+
+function getFirstNumberRepeatedLogFilenames(arrFilenamesRepeated) {
+	let smallerNumber = MAX_LOGS;
+	let arrOrderedExistingFileNumber = new Array(MAX_LOGS);
+	arrOrderedExistingFileNumber.fill(false);
+
+	arrFilenamesRepeated.forEach(filename => {
+		let fileNum = parseInt(filename.match(/.(\d*?).log/)[1]);
+		if(!isNaN(fileNum)) {
+			arrOrderedExistingFileNumber[fileNum] = true;
+		}
+	});
+	for(let i = 0; i < arrOrderedExistingFileNumber.length; i++) {
+		if(arrOrderedExistingFileNumber[i] == false) {
+			smallerNumber = i;
+			break;
+		}
+	}
+
+	return smallerNumber;
+}
+
+function renameLogForArchiving(logDirectory, filePathToRename) {
+	let fileLastModifyDate = new Date(fs.statSync(filePathToRename).mtimeMs);
+	let currentTimeFormated = fileLastModifyDate.getFullYear() + '-' + fillLeftZero(String(fileLastModifyDate.getMonth()), 2) + '-' + fillLeftZero(String(fileLastModifyDate.getDate()), 2);
+
+	let fileNamesRepeated = getLogFilesNamesContainsString(logDirectory, currentTimeFormated);
+	let smallerNumberNonExistent = getFirstNumberRepeatedLogFilenames(fileNamesRepeated);
+
+	fs.renameSync(filePathToRename, logDirectory + LOG_ARCHIVE_PREFIX + currentTimeFormated + '.' + smallerNumberNonExistent + LOG_SUFIX);
+}
+
 exports.initLogger = function(logDir, logLevel) {
 	logDirectory = logDir;
 	currentLogLevel = logLevel;
+	currentLogfileName = CURRENT_LOG_NAME + LOG_SUFIX;
 
 	// Create directory if doesn't exist
 	if (!fs.existsSync(logDirectory)){
 		fs.mkdirSync(logDirectory);
 	}
 
-	let currentTime = new Date();
-	let currentTimeFormated = currentTime.getFullYear() + '-' + fillLeftZero(String(currentTime.getMonth()), 2) + '-' + fillLeftZero(String(currentTime.getDate()), 2);
+	let allLogsFileNames = getLogFileNames(logDirectory);
 
-	let arrFilesInLogDir = fs.readdirSync(logDirectory);
-
-	// Count num of logs
-	let allLogsFileNames = arrFilesInLogDir.filter(fileName => {
-		if(fileName.indexOf('.log') != -1)return true;
-	});
-	let numLogs = allLogsFileNames.length;
-
-	// Remove one old file
-	if(numLogs >= MAX_LOGS) {
-		let oldestFile = null;
-		let oldestfileModificationTime = null;
-		allLogsFileNames.forEach(file => {
-			let fileModificationTime = fs.statSync(logDirectory + file).mtimeMs;
-			if(oldestfileModificationTime == null || fileModificationTime < oldestfileModificationTime) {
-				oldestFile = file;
-				oldestfileModificationTime = fileModificationTime;
-			}
-		});
-		fs.unlinkSync(logDirectory + oldestFile);
+	// Remove oldest log file if we reached MAX_LOGS
+	if(allLogsFileNames.length >= MAX_LOGS) {
+		removeOldestLog(logDirectory, allLogsFileNames);
 	}
 
-	// Count repeated log files
-	arrFilesInLogDir = fs.readdirSync(logDirectory);
-	allLogsFileNames = arrFilesInLogDir.filter(fileName => {
-		if(fileName.indexOf('.log') != -1)return true;
-	});
-	let repeatedTodayLogFiles = allLogsFileNames.filter(fileName => {
-		if(fileName.indexOf(LOGGIN_FILE_NAME + currentTimeFormated) != -1)return true;
-	});
+	let currentLogPath = logDirectory + currentLogfileName;
 
-	// Get first number
-	let smallerAvalibleFileNum = 0;
-	let arrExistingNum = new Array(MAX_LOGS + 1);
-	arrExistingNum.fill(false);
-	repeatedTodayLogFiles.forEach(fileName => {
-		let fileNum = parseInt(fileName.match(/.(\d*?).log/)[1]);
-		if(!isNaN(fileNum)) {
-			arrExistingNum[fileNum] = true;
-		}
-	});
-	for(let i = 0; i < arrExistingNum.length; i++) {
-		if(!arrExistingNum[i]) {
-			smallerAvalibleFileNum = i;
-			break;
-		}
+	if(fs.existsSync(currentLogPath)) {
+		// Rename it with last date update
+		renameLogForArchiving(logDirectory, currentLogPath)
 	}
-
-	currentLogfileName = LOGGIN_FILE_NAME + currentTimeFormated + '.' +  smallerAvalibleFileNum + '.log';
 	
 	// Init new file
-	fs.writeFileSync(logDirectory + currentLogfileName, '');
+	fs.writeFileSync(currentLogPath, '');
 }
 
 exports.log = function(logType, strData) {
@@ -91,8 +117,10 @@ exports.log = function(logType, strData) {
 		let info = '[' + currentDateTime + ']';
 		let strToLog =  info + ' ' + strData;
 
+		let currentLogPath = logDirectory + currentLogfileName;
+
 		console.log(strToLog);
-		fs.writeFileSync(logDirectory + currentLogfileName, strToLog + '\n', { flag: 'a' });
+		fs.writeFileSync(currentLogPath, strToLog + '\n', { flag: 'a' });
 	}
 }
 
