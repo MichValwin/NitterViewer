e69f6bf319b4ee55d23e4a179da7d3a05b4f8d4b
MichaelValwin <macm241192@gmail.com>
2022-10-09 16:59:37 +0200

    Change dev settings again

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: 8020,
 	API_ENDPOINT: '/v1',
 	LOG_DIRECTORY: './logs/',
-	LOG_LEVEL: 4,
+	LOG_LEVEL: 3,
 
 	PASSWORD_FILE: './password.txt',
 	SALT_ROUNDS: 14,
@@ -11,7 +11,7 @@ module.exports = {
 
 	SOCKS_PROXY: '', // Ex: 'socks5://192.168.1.151:9050'
 
-	NITTER_WEBSITE: 'https://nitter.privacy.com.de',
+	NITTER_WEBSITE: 'https://nitter.pussthecat.org',
 	TWITTER_LISTS: './nitterList.json',
 
 	COOKIE_MAX_AGE: 1000 * 60 * 60 * 24 * 30, // 30 days
